1f84f51dc508ea3dbc90ce61a5cb78d0695537f2
Victor Herrera <vic_h_o@hotmail.com>
2026-03-08 18:25:00 -0600

    Task 1.2: Extract core logic

---
+++ packages/core/src/index.ts
@@ -0,0 +1,5 @@
+export * from './models/task.js';
+export * from './models/artifact.js';
+export * from './services/taskManager.js';
+export * from './services/artifactRegistry.js';
+export * from './services/artifactService.js';
